e3ceecfbc2d431fc5411633bf5b84bf584c68c42
Md Jakaria Hasan <141640103+jakariasks@users.noreply.github.com>
2024-07-04 20:32:11 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,54 @@
+const hours = document.querySelector('#hours');
+const minutes = document.querySelector('#minutes');
+const seconds = document.querySelector('#seconds');
+const period = document.querySelector('#period');
+const day = document.querySelector('#day');
+const date = document.querySelector('#date')
+const month = document.querySelector('#month')
+const year = document.querySelector('#year');
+
+const days = ['SUN','MON','TUE','WED','THU','FRI','SAT'];
+const months = ['JAN','FEB','MARCH','APRIL','MAY','JUNE','JULY','AUG','JAN','SEP','OCT','NOV','DEC'];
+
+
+function clock() {
+    const time = new Date();
+
+    let h = time.getHours();
+
+    if (h >= 12) {
+        hours.innerHTML = h === 12 ? 12 : h - 12;
+        period.innerHTML = 'PM';
+    } else {
+        hours.innerHTML = h === 0 ? 12 : h;
+        period.innerHTML = 'AM';
+    }
+
+    let m = time.getMinutes();
+    minutes.innerHTML = m;
+
+    let s = time.getSeconds()
+    seconds.innerHTML = s;
+
+    let d = time.getDay();
+    days.forEach( (e, i,array) => {
+        console.log(days[d])
+        day.innerHTML = days[d];
+    });
+
+    let dt = time.getDate();
+    date.innerHTML = dt;
+
+
+    let mt = time.getMonth();
+    months.forEach( (e, i,array) => {
+        console.log(months[mt])
+        month.innerHTML = months[mt];
+    });
+
+    let y = time.getFullYear();
+    year.innerHTML = y;
+    
+}
+
+setInterval(clock, 1000);
